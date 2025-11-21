1adc960e86bba18ffc495f6f6473a855601e1927
Shoaib Taimur <mitashoaib@gmail.com>
2025-11-21 21:25:30 +0600

    Finished

--- script/toggle.js
+++ script/toggle.js
@@ -8,3 +8,8 @@ document.getElementById('cash-out-box').addEventListener('click', function (even
     document.getElementById('cash-out').style.display = 'block';
     document.getElementById('add-money').style.display = 'none';
 })
+
+document.getElementById('logout-btn').addEventListener('click',function()
+{
+    window.location.href="./index.html";
+})
